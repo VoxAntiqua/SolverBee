7bce60f9711ee5975781f69066a531e62d24ef58
Andrew Padgett <andrew.padgett.88@gmail.com>
2020-08-04 13:33:36 -0400

    Initial commit

---
+++ app/src/main/java/com/andrew_padgett/solverbee/ui/input/InputViewModel.kt
@@ -0,0 +1,7 @@
+package com.andrew_padgett.solverbee.ui.input
+
+import androidx.lifecycle.ViewModel
+
+class InputViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/andrew_padgett/solverbee/ui/results/ResultsViewModel.kt
@@ -0,0 +1,7 @@
+package com.andrew_padgett.solverbee.ui.results
+
+import androidx.lifecycle.ViewModel
+
+class ResultsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
