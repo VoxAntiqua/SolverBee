a53fabe41cb22337d1c7dec1c35467ac825c3a43
Andrew Padgett <andrew.padgett.88@gmail.com>
2020-08-05 14:06:21 -0400

    added word list asset

--- app/src/main/java/com/andrew_padgett/solverbee/ui/input/InputFragment.kt
+++ app/src/main/java/com/andrew_padgett/solverbee/ui/input/InputFragment.kt
@@ -32,15 +32,12 @@ class InputFragment : Fragment() {
         val inputViewModel = ViewModelProvider(this, inputViewModelFactory).get(InputViewModel::class.java)
 
         binding.inputViewModel = inputViewModel
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
 
         inputViewModel.navigateToResults.observe(viewLifecycleOwner, Observer {
             if (it == true) {
                 this.findNavController().navigate(InputFragmentDirections.actionInputFragmentToResultsFragment(inputViewModel.requiredLetter.value!!, inputViewModel.availableLetters.value!!))
-                Snackbar.make(activity?.findViewById(android.R.id.content)!!, "navigateToResults is true", Snackbar.LENGTH_SHORT).show()
                 inputViewModel.doneNavigating()
-            } else if (it == false) {
-                Snackbar.make(activity?.findViewById(android.R.id.content)!!, "Incorrect input", Snackbar.LENGTH_SHORT).show()
             }
         })
 
